10e115bb315bb5748987b9942a8e8bc394add125
nhs0912 <nhs0912@nate.com>
2025-12-23 17:22:15 +0900

    first commit

---
+++ src/main/java/com/justone/justone_batch/JustoneBatchApplication.java
@@ -0,0 +1,13 @@
+package com.justone.justone_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustoneBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustoneBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justone/justone_batch/JustoneBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justone.justone_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustoneBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
